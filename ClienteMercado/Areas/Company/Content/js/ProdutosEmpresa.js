d56b5603c2c297a4d677b92e76fed364892a3680
Edwilson Curti <edwilsoncurti@hotmail.com>
2021-03-06 16:56:29 -0300

    - Criados botões que direcionam para o cadastro em cada página do SuperMarketOn; - Criados botões nas páginas de Cadastro em todo o SuperMarketOn, botão pra retorno (já funcionando) e botão de Gravação (ainda a funcionar); - Inserido códs. JS para os botões principais (Cadastro e Voltar) para todas as páginas do SuperMarketOn; - Inserido código Html Razor base em todas as páginas de Cadastro do SuperMarketOn.

--- ClienteMercado/Areas/Company/Content/js/ProdutosEmpresa.js
+++ ClienteMercado/Areas/Company/Content/js/ProdutosEmpresa.js
@@ -1,3 +1,19 @@
 ﻿$(document).ready(function () {
+    var url1 = $('#Url1').val();
+    var url2 = $('#Url2').val();
 
+    //BOTÃO NOVO CADASTRO
+    $(document).on("click", "#btn-cadastrar", function () {
+        debugger;
+
+        //REDIRECIONA NOVO CADASTRO
+        window.location.href = url1;
+    });
+
+    //BOTÃO VOLTAR PÁGINA ANTERIOR
+    $(document).on("click", "#btn-voltar", function () {
+        debugger;
+
+        window.location.href = url2;
+    });
 });
